f751df69ec91a9b058e79b61fde4f60b02e5f837
pratikshadighe <pratiksha.d@girmesofttech.com>
2023-11-29 12:16:38 +0530

    "send mail changes"

--- utils/sendmail.js
+++ utils/sendmail.js
@@ -2,8 +2,8 @@ import { createTransport } from "nodemailer";
 
 export const sendMail = async (email, subject, text) => {
   const transport = createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
+    // host: process.env.SMTP_HOST,
+    // port: process.env.SMTP_PORT,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
