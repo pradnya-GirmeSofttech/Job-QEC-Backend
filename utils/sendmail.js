ab8ba9ffe78a756efba191862657515456573fd3
pratikshadighe <pratiksha.d@girmesofttech.com>
2023-11-29 12:21:01 +0530

    "send Mail"

--- utils/sendmail.js
+++ utils/sendmail.js
@@ -4,6 +4,7 @@ export const sendMail = async (email, subject, text) => {
   const transport = createTransport({
     // host: process.env.SMTP_HOST,
     // port: process.env.SMTP_PORT,
+    service: "gmail",
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
